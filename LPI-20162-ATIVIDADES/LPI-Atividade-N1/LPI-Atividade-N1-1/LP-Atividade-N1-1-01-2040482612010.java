13a665630f419b9423df50983c6b9248ea441816
Angelina N Busch <angelinanovaesbusch@gmail.com>
2026-08-14 14:30:59 -0300

    Entrega das Atividades N-1

--- LPI-20162-ATIVIDADES/LPI-Atividade-N1/LPI-Atividade-N1-1/LP-Atividade-N1-1-01-2040482612010.java
+++ LPI-20162-ATIVIDADES/LPI-Atividade-N1/LPI-Atividade-N1-1/LP-Atividade-N1-1-01-2040482612010.java
@@ -2,32 +2,21 @@ import java.util.Scanner;
 public class Primitivos {
     public static void main(String[]args) {
         Scanner sc = new Scanner(System.in);
-
-         //byte: 8 bits | intervalo: -128 a 127
+        
         byte idade;
 
-        //short: 16 bits | intervalo: -32.768 a 32.767
         short faltas;
 
-        //int: 32 bits | intervalo: -2.147.483.648 a 2.147.483.647
         int matriculaId;
 
-        //long: 64 bits | intervalo: -9.223.372.036.854.775.808
-        // a 9.223.372.036.854.775.807
         long codigoNacional;
-
-        //float: 32 bits | aproximadamente ±3,4 × 10^38
-        //(precisão de aproximadamente 6-7 casas decimais)
+        
         float notaTrabalho;
 
-        //double: 64 bits | aproximadamente ±1,7 × 10^308
-        //(precisão de aproximadamente 15-16 casas decimais)
         double notaProvaFinal;
 
-        //char: 16 bits | intervalo: '\u0000' (0) a '\uFFFF' (65.535)
         char conceitoFinal;
 
-        //boolean só armazena true ou false, por isso não possui intervalo definido
         boolean aprovado;
 
 
