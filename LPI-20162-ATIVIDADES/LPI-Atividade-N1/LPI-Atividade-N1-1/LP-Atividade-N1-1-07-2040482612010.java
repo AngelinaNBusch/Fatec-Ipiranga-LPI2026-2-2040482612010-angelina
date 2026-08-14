a9cdda7e4356e3723d953febd74e3b953705bb60
Angelina N Busch <angelinanovaesbusch@gmail.com>
2026-08-14 14:19:31 -0300

    Entrega das Atividades N-1

---
+++ LPI-20162-ATIVIDADES/LPI-Atividade-N1/LPI-Atividade-N1-1/LP-Atividade-N1-1-07-2040482612010.java
@@ -0,0 +1,44 @@
+import java.util.Scanner;
+import java.math.BigDecimal;
+import java.math.RoundingMode;
+public class precisaoDecimal {
+    public static void main(String[] args) { 
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Informe o valor A (ex: 1.00): ");
+        String valorA = sc.nextLine();
+        System.out.print("Informe o valor B (ex: 0.90): ");
+        String valorB = sc.nextLine();
+        System.out.print("Informe o valor total da compra: ");
+        String valorTotal = sc.nextLine();
+        System.out.print("Informe o número de parcelas: ");
+        int numParcelas = Integer.parseInt(sc.nextLine());
+
+        //Forma exata usando java.math.BigDecimal
+
+        BigDecimal bigA = new BigDecimal(valorA);
+        BigDecimal bigB = new BigDecimal(valorB);
+        BigDecimal resultadoPreciso = bigA.subtract(bigB);
+
+        double doubleA = Double.parseDouble(valorA);
+        double doubleB = Double.parseDouble(valorB);
+        double resultadoErrado = doubleA - doubleB;
+
+        System.out.println("--- Demonstração da imprecisão do padrão IEEE 754 (double) ---");
+        System.out.println("Resultado esperado de " + valorA + " - " + valorB + " seria: " + resultadoPreciso);
+        System.out.println("Resultado real obtido com double: " + resultadoErrado);
+
+        System.out.println("--- Correção exata usando java.math.BigDecimal ---");
+        System.out.println("Resultado com BigDecimal (String Constructor): " + resultadoPreciso);
+
+        //Divisão de parcelas
+        BigDecimal bigCompra = new BigDecimal(valorTotal);
+        BigDecimal valorParcela = bigCompra.divide(BigDecimal.valueOf(numParcelas), 2, RoundingMode.HALF_UP);
+
+        System.out.println("--- Divisão de parcelas com Escala e RoundingMode.HALF_UP ---");
+        System.out.println("Compra de R$ " + valorTotal + " dividida em " + numParcelas + "x: R$ " + valorParcela + " por parcela.");
+
+        sc.close();
+
+    }
+}
