6d6cca04b9721ef8e48ba3e8e12803424d24ac2c
Angelina N Busch <angelinanovaesbusch@gmail.com>
2026-08-20 17:40:33 -0300

    Entrega de Atividades N-1

--- LPI-20162-ATIVIDADES/LPI-Atividade-N1/LPI-Atividade-N1-1/LP-Atividade-N1-1-06-2040482612010.java
+++ LPI-20162-ATIVIDADES/LPI-Atividade-N1/LPI-Atividade-N1-1/LP-Atividade-N1-1-06-2040482612010.java
@@ -16,6 +16,14 @@ public class Curto {
 
         boolean valido = texto != null && texto.length() > 0;
 
+      // O operador & sempre verifica as duas condições da expressão, mesmo quando a primeira
+      // já seria suficiente para determinar o resultado. Por exemplo, se a variável texto for
+      // null, o Java ainda vai tentar executar texto.length(), causando um NullPointerException,
+      // pois não é possível chamar um método em uma referência nula.
+      // Já o operador && funciona de maneira diferente. Ele só verifica a segunda condição se
+      // a primeira for verdadeira. Assim, quando a primeira condição já for falsa, o Java para
+      // a avaliação naquele momento e não executa o restante da expressão, evitando o erro.
+
         if (valido) {
             System.out.println("Usuário Válido Fornecido: " + texto);
         } else { 
